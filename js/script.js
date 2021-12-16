e625ba3eabde50548538928959bd7998ccab5db8
Saifi-6397 <khaleelahmad3886@gmail.com>
2021-12-16 15:40:55 +0530

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(function() {
     $(".nav-btn").click(function() {
         $("header nav li").slideToggle();
     });
+
+    //scroll up to top 
     $(window).on("scroll", function() {
         if ($(window).scrollTop()) {
             $("header").addClass('scroll')
@@ -9,7 +11,14 @@ $(function() {
             $('header').removeClass('scroll');
         }
     });
+    // hover image
 
+    $(".product-section .box").hover(function() {
+        $("#text-1").slideToggle('slow');
+        // $(".icons").show();
+    })
+
+    //aos animation
     AOS.init({
         duration: 1000,
         delay: 400,
@@ -17,3 +26,39 @@ $(function() {
     });
 
 })
+
+//validation
+var user_name = document.getElementById("name");
+var user_email = document.getElementById("email");
+var user_phone = document.getElementById("phone");
+var return_val = 1;
+
+function Validation() {
+    if (user_name.value == "") {
+        document.getElementById("name_error").innerHTML = "Please Entter Your Name";
+        return_val = 0;
+    } else {
+        document.getElementById("name_error").innerHTML = "";
+        return_val = 1;
+    }
+    if (user_email.value == "") {
+        document.getElementById("email_error").innerHTML = "Please Entter Your Email";
+        return_val = 0;
+    } else {
+        document.getElementById("email_error").innerHTML = "";
+        return_val = 1;
+    }
+    if (user_phone.value == "") {
+        document.getElementById("phone_error").innerHTML = "Please Entter Your Phone No";
+        return_val = 0;
+    } else {
+        document.getElementById("phone_error").innerHTML = "";
+        return_val = 1;
+    }
+
+    if (return_val) {
+        return true;
+    } else {
+        return false;
+    }
+}
